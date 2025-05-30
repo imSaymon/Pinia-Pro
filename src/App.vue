8965bfd22665e9ee4b838bf6a0f0424bb9b74d14
Saymon Rodrigues <saymonlee25@hotmail.com>
2025-05-29 22:12:41 -0300

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-layout>
+      <v-app-bar class="text-center">
+        <v-app-bar-title class="text-h5 font-weight-bold">Nova ordem de serviço</v-app-bar-title>
+
+
+        <template v-slot:append>
+          <v-btn
+            icon="mdi-bug"
+            :color="debug ? 'pink' : 'black'"
+            @click="debug = !debug"
+          />
+        </template>
+      </v-app-bar>
+
+      <v-main class="mt-6">
+        <v-container>
+          <v-row>
+            <v-col>
+              <h3>Organizações Tabajara</h3>
+              Avenida Dom Eugênio Sales, 96 - Boca do Rio <br>
+              Salvador - Bahia 41715-030 <br>
+              Tel: 71 9999-9999 <br>
+              E-mail: contato@tabajara.com.br
+            </v-col>
+
+            <v-col>
+              <OSDetails/>
+            </v-col>
+          </v-row>
+
+          <div class="mt-8">
+            <OSClient/>
+          </div>
+
+          <div class="mt-8">
+            <OSServices/>
+          </div>
+
+          <div class="mt-6">
+            <v-row>
+              <v-col>
+                <v-textarea
+                  v-model="description"
+                  label="Descrição da ordem de serviço"
+                  hide-details
+                  variant="outlined"
+                />
+              </v-col>
+
+              <v-col>
+                <OSTotal/>
+              </v-col>
+            </v-row>
+          </div>
+
+          <div class="mt-8 text-right">
+            <v-btn variant="text">Cancelar</v-btn>
+            <v-btn color="primary" @click="onSubmit">Enviar</v-btn>
+          </div>
+
+          <template v-if="debug">
+            <hr class="my-6">
+            <b>Detalhes:</b> {{orderDetails}} <br>
+            <b>Cliente:</b> {{client}} <br>
+            <b>Servicos:</b> {{services}} <br>
+            <b>Descrição:</b> {{description}} <br>
+            <b>Tipo de Desconto:</b> {{discountType}} <br>
+            <b>Valor do Desconto:</b> {{discountValue}} <br>
+            <b>Subtotal:</b> {{subtotal}} <br>
+            <b>Total:</b> {{total}} <br>
+          </template>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-app>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import {useServiceOrderStore} from './store/serviceOrder.js';
+import {ref} from 'vue';
+import OSDetails from './components/OSDetails.vue';
+import OSClient from './components/OSClient.vue';
+import OSServices from './components/OSServices.vue';
+import OSTotal from './components/OSTotal.vue';
+import {storeToRefs} from 'pinia';
+
+const serviceOrderStore = useServiceOrderStore()
+const {
+  orderDetails,
+  client,
+  services,
+  description,
+  discountType,
+  discountValue,
+  subtotal,
+  total,
+} = storeToRefs(serviceOrderStore)
+const debug = ref(false)
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import {createPinia} from 'pinia';
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-const pinia = createPinia();
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
-createApp(App).use(pinia).mount("#app");
+const pinia = createPinia()
+
+createApp(App)
+  .use(pinia)
+  .use(vuetify)
+  .mount('#app')
